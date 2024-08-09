43e93cfb8ec370b0c678ff778e19b0d7bb1f92c2
EvananSemprun <80002503+EvananSemprun@users.noreply.github.com>
2024-08-09 15:30:10 -0400

    Merge #8 developing 1.0.6.3-alpha

--- src/components/rifamax/home/StepperRaffle.tsx
+++ src/components/rifamax/home/StepperRaffle.tsx
@@ -1,9 +1,28 @@
 import AddRaffleForm from '@/form/rifamax/Home/AddRaffle.form';
 import { useState } from 'react';
 import { Stepper } from '@mantine/core';
+import TicketRaffle from './TicketRaffle';
+
+// const raffle = {
+//   numbers: '1234',
+//   price: 500,
+//   currency: 'USD',
+//   prizes: [
+//     { award: 'Carro del año' }, 
+//     { award: 'Moto' }           
+//   ],
+//   id: '56789',
+//   lotery: 'Lotería Nacional',
+//   init_date: '2024-08-10',
+//   expired_date: '2024-09-10',
+//   seller: {
+//     name: 'Juan Pérez',
+//     phone: '+1 555-1234'
+//   }
+// };
 
 function StepperRaffle() {
-  const [active, setActive] = useState(0);
+  const [active, setActive] = useState(2);
   const nextStep = () => setActive((current) => (current < 2 ? current + 1 : current));
   const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
 
@@ -13,7 +32,7 @@ function StepperRaffle() {
         <AddRaffleForm onNext={nextStep} onBack={prevStep} />
       </Stepper.Step>
       <Stepper.Step label="Verificación" description="Verificación que los datos de la rifa sean correctos">
-        Step 2 content: Verify email
+        {/* <TicketRaffle raffle={raffle} /> */}
       </Stepper.Step>
       <Stepper.Completed>
         Completed, click back button to get to previous step
---
+++ src/components/rifamax/home/TicketRaffle.tsx
@@ -0,0 +1,46 @@
+
+import CardTitle from './CardTitle';
+import { ICardRaffle } from '@interfaces/index';
+import { Button, Text, Divider, Card, Image, Center } from '@mantine/core'
+
+function TicketRaffle({ raffle }: ICardRaffle) {
+  return (
+    <Card radius='md' shadow='xl' maw={350} w="100%">
+      <Center>
+        <Image
+          mt={15}
+          src="/images/rifamax-logo.png"
+          alt="Rifamax Logo"
+          maw='15rem' 
+          py={40}
+        />
+      </Center>
+  
+      <Center mt={10}>
+        <Text fw={700} size="xl">
+          {raffle.numbers} - SIGNO
+        </Text>
+      </Center>
+  
+      <Center>
+        <Text fw={700} size="xl">
+          Precio: {raffle.price} {raffle.currency}
+        </Text>
+      </Center>
+  
+      <Divider my={10} color='white' size="xs" />
+      <CardTitle title='Premio' content={raffle.prizes[0]?.award || 'No disponible'} />
+      <CardTitle title='Premio sin signo' content={raffle.prizes[1]?.award || 'No disponible'} />
+      <Divider mt={10} mb={10} color='white' size="xs" />
+      
+      <CardTitle title='SERIE NUMERO:' content={raffle.id} />
+      <CardTitle title='LOTERIA:' content={raffle.lotery} />
+      <CardTitle title='FECHA:' content={raffle.init_date} />
+      <CardTitle title='CADUCA:' content={raffle.expired_date} />
+      <CardTitle title='RIFERO:' content={raffle.seller.name} />
+      <CardTitle title='TELEFONO:' content={raffle.seller.phone} />
+    </Card>
+  )
+}
+
+export default TickteRaffle
