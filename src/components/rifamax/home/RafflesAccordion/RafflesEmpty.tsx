c84e94718732513fc46a4c4ce89253764e6fff1d
Javier Diaz <javierdiazt406@gmail.com>
2024-08-12 10:49:09 -0400

    Version 1.0.6.3-alpha ready to pre-release

---
+++ src/components/rifamax/home/RafflesAccordion/RafflesEmpty.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@mantine/core"
+
+function RafflesEmpty() {
+  return (
+    <Text mt={40} ta="center" fz={26} fw={300}>
+      No hay rifas disponibles
+    </Text>
+  )
+}
+
+export default RafflesEmpty
