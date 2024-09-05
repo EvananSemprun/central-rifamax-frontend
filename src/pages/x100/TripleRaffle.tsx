f791a47657b6515d2488da41b1610a523d0641ae
EvananSemprun <80002503+EvananSemprun@users.noreply.github.com>
2024-09-05 16:27:45 -0400

    Merge #30: Added URL params to TriplesRaffles

--- src/pages/x100/TripleRaffle.tsx
+++ src/pages/x100/TripleRaffle.tsx
@@ -1,15 +1,19 @@
-import { Card, Center, Text } from "@mantine/core"
+import { useParams } from "react-router-dom";
+import { Card, Center, Text } from "@mantine/core";
 
 function TripleRaffle() {
+  const { integrator, playerId, raffleId, currency } = useParams();
+
   return (
     <Center>
       <Card w={500} h={300} mt={20}>
-        <Text></Text>
-        <Text></Text>
-        <Text></Text>
+        <Text>{integrator}</Text>
+        <Text>payer: {playerId}</Text>
+        <Text>Raffle ID: {raffleId}</Text>
+        <Text>Currency: {currency}</Text>
       </Card>
     </Center>
-  )
+  );
 }
 
-export default TripleRaffle
+export default TripleRaffle;
