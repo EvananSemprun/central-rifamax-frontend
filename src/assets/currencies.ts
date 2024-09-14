e5198f722998acb30033dd23a02a9c001ba88d3b
Javier Diaz <javierdiazt406@gmail.com>
2024-09-14 15:34:29 -0400

    feat(currency): added new object method currencies

---
+++ src/assets/currencies.ts
@@ -0,0 +1,14 @@
+export const currencies = [
+  {
+    label: 'Dólares Americanos',
+    value: 'USD',
+  },
+  {
+    label: 'Bolívares Digitales',
+    value: 'VES',
+  },
+  {
+    label: 'Pesos Colombianos',
+    value: 'COP',
+  }
+]
