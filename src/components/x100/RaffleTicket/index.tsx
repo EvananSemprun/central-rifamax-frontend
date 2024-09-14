e2183aeb42607be26badcd5086493a2c046d8f7f
Javier Diaz <javierdiazt406@gmail.com>
2024-09-14 17:45:32 -0400

    feat: entry point of triples page and integration compatibility

--- src/components/x100/RaffleTicket/index.tsx
+++ src/components/x100/RaffleTicket/index.tsx
@@ -1,13 +1,28 @@
+import { Button, Text } from "@mantine/core"
+import { toBet } from "@utils/transform"
 import { IRaffleTicket } from "@interfaces/index"
 
 function index({
   value,
   betType,
   select = true,
+  className,
   onSelect
 }: IRaffleTicket) {
   return (
-    <div>index</div>
+    <Button 
+      disabled={!select} 
+      className={className}
+      size="lg"
+      radius="lg"
+      onSelect={() => {
+        select ? onSelect : undefined
+      }}
+    >
+      <Text fw={350} fz={25}>
+        {toBet({ value, betType })}  
+      </Text>
+    </Button>
   )
 }
 
--- src/pages/x100/TripleRaffle.tsx
+++ src/pages/x100/TripleRaffle.tsx
@@ -1,15 +1,27 @@
-import IntegratorInfo from "@components/x100/IntegratorInfo"
-import { useParams } from "react-router-dom";
-import { Card, Center, Text } from "@mantine/core";
-import { MoneyType } from "@interfaces/models.interfaces";
+import classes from './TripleRaffle.module.css'
+import TouchBar from '@components/x100/Touchbar'
+import useIntegrator from '@hooks/useIntegrator'
+import IntegratorInfo from '@components/x100/IntegratorInfo'
+import { useState } from 'react'
+import { MoneyType } from '@interfaces/models.interfaces'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useViewportSize } from '@mantine/hooks'
 
 function TripleRaffle() {
+  const navigate = useNavigate();
+  const dataIntegrator = useIntegrator().integrator;
+
+  const { width } = useViewportSize();
+
   const { integrator, playerId, raffleId, currency } = useParams();
 
-  if (raffleId === 'null') {
-    <Text>
-      awdaw
-    </Text>
+  const [currencySelected, setCurrencySelected] = useState<MoneyType>(currency as MoneyType || 'USD')
+
+  const changeExchange = (value: MoneyType) => {
+    setCurrencySelected(value)
+    if (dataIntegrator.player_id > 0) {
+      return navigate(`/x100/i/${integrator}/p/${playerId}/c/${value}/raffle/${raffleId}`)
+    }
   }
 
   return (
@@ -18,16 +30,61 @@ function TripleRaffle() {
         currency={currency as MoneyType}
         integratorToken={integrator}
         playerId={playerId}
-        integrator="CDA"
+        integrator='CDA'
       />
-      <Center>
-        <Card w={500} h={300} mt={20}>
-          <Text>{integrator}</Text>
-          <Text>Player {playerId}</Text>
-          <Text>Raffle ID: {raffleId}</Text>
-          <Text>Currency: {currency}</Text>
-        </Card>
-      </Center>
+      <div className={classes.infoContainer}>
+        <div className={classes.infoWrapper}>
+          <TouchBar 
+            pagyButton={{
+              color: 'blue',
+              variant: 'light',
+              disabledUp: true,
+              disabledDown: false,
+              onPressedUp: () => {},
+              onPressedDown: () => {},
+            }}
+            currencyButton={{
+              size: 'sm',
+              color: 'blue',
+              borders: false,
+              bg: 'transparent',
+              defaultCurrency: currencySelected,
+              onChange(value) {
+                changeExchange(value)
+              },
+            }}
+            combosButtons={{
+              data: [
+                {
+                  price: 2,
+                  value: 4
+                },
+                {
+                  price: 3,
+                  value: 8
+                }
+              ],
+              onSelect(price, value) {
+                console.log('price', price)
+                console.log('value', value)
+              },
+            }}
+            priceText={{
+              price: 4
+            }}
+          />
+        </div>
+      </div>
+      <div className={integrator ? classes.integratorWrapper : classes.wrapper}>
+        <div className={classes.ticketsWrapper}>
+        </div>
+        { width >= 1150 && (
+            <div className={classes.resultWrapper}>
+              {/* Ticket order selected */}
+            </div> 
+          )
+        }
+      </div>
     </>
   );
 }
