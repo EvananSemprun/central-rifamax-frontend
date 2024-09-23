8e33c039152d189ca9743fdb565236671b8455da
Javier Diaz <70349374+javierd79@users.noreply.github.com>
2024-09-23 18:35:28 -0400

    refactor: Deleted useEffect and change useMutation to useQuery

--- src/components/rifamax/home/TableRaffle.tsx
+++ src/components/rifamax/home/TableRaffle.tsx
@@ -1,34 +1,23 @@
-import React from 'react';
 import useAuth from '@hooks/useAuth';
-import { capitalize } from '@utils/string';
-import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 import { AxiosResponse } from 'axios';
-import { getCloseDayRaffles } from '@/api/rifamax/Raffles.request';
+import { capitalize } from '@utils/string';
+import { useQuery } from '@tanstack/react-query';
 import { adminStatus, sellStatus } from '@utils/parse';
 import { Table, Badge, ScrollArea } from '@mantine/core';
-import { ICloseDayResponse, IUnclosedRaffle } from '@/interfaces/requests.interfaces';
+import { getCloseDayRaffles } from '@api/rifamax/Raffles.request';
+import { LoaderBlur } from '@components/shared/Loaders/LoaderBlur';
+import { ICloseDayResponse, IUnclosedRaffle } from '@interfaces/requests.interfaces';
 
 function TableRaffle() {
   const { token } = useAuth();
 
-  const mutation = useMutation<AxiosResponse<ICloseDayResponse>>({
-    mutationFn: () => getCloseDayRaffles(token),
-    onSuccess: (response) => {
-      const unclosedRaffles = response.data.unclosed;
-      setRaffles(unclosedRaffles);
-    },
-    onError: (error) => {
-      console.error("Error fetching raffles:", error);
-    }
+  const { isSuccess, isError, isLoading, data: request } = useQuery<AxiosResponse<ICloseDayResponse>>({
+		queryKey: ['get', 'unclosed'],
+    queryFn: () => getCloseDayRaffles(token)
   });
 
-  const [raffles, setRaffles] = React.useState<IUnclosedRaffle[]>([]);
-
-  React.useEffect(() => {
-    mutation.mutate();
-  }, []);
-
-  const rows = raffles.map((raffle) => (
+  const rows = request.data.unclosed.map((raffle) => (
     <Table.Tr ta="center" key={raffle.id}>
       <Table.Td>{raffle.id}</Table.Td>
       <Table.Td>{capitalize(sellStatus(raffle.sell_status))}</Table.Td>
@@ -43,19 +32,25 @@ function TableRaffle() {
 
   return (
     <ScrollArea type="auto" w="100%" h={205} style={{ maxWidth: '100%', overflowX: 'auto' }}>
-      <Table mt={20} mb={20} striped highlightOnHover withTableBorder>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th ta='center'>Serie</Table.Th>
-            <Table.Th ta='center'>Estado</Table.Th>
-            <Table.Th ta='center'>Verificación</Table.Th>
-            <Table.Th ta='center'>Monto</Table.Th>
-            <Table.Th ta='center'>Rifero</Table.Th>
-            <Table.Th ta='center'>Fecha</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
+				{ 
+					isLoading ? (
+						<LoaderBlur label="Cargando rifas..." />
+					) : (
+					<Table mt={20} mb={20} striped highlightOnHover withTableBorder>
+		        <Table.Thead>
+		          <Table.Tr>
+		            <Table.Th ta='center'>Serie</Table.Th>
+		            <Table.Th ta='center'>Estado</Table.Th>
+		            <Table.Th ta='center'>Verificación</Table.Th>
+		            <Table.Th ta='center'>Monto</Table.Th>
+		            <Table.Th ta='center'>Rifero</Table.Th>
+		            <Table.Th ta='center'>Fecha</Table.Th>
+		          </Table.Tr>
+		        </Table.Thead>
+		        <Table.Tbody>{rows}</Table.Tbody>
+		      </Table>
+				)
+			}
     </ScrollArea>
   );
 }
